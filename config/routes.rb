0d4099f1a2d7e4798c8f8233f43006baf41ad3d2
AlecksJohanssen <minh.dq1078@sinhvien.hoasen.edu.vn>
2017-02-07 14:26:24 +0700

    Add Subdomains

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
+  constraints :subdomain => 'api' do
+	  namespace :api, path: nil, defaults: {format: 'json'} do
+	    namespace :v1 do
+	    	resources :users
+	    end
+	  end
+	end
 end
